ffd526403d4868985e00c398d383c319969df330
tom-cf <uipps_com@163.com>
2022-02-26 15:45:10 +0800

    一些小修改,先提交一版；去掉一些不用的，准备重构!

--- morenfun.cls.php
+++ morenfun.cls.php
@@ -1,36 +1,204 @@
 <?php
+/**
+ 6个面顺序参考： F:/develope/python/game/mofang_rubikcube/rubiksCube_AlphaZero 的enums.py中的图案,
+     (git@github.com:uipps/AI_AlphaZero.git 的 rubiksCube分支)
 
-// 魔方对象，存储魔方的状态。一共6+12=8=26个
-$m = [
-    'd'=>   'w',   'u'=>   'y',   'l'=>   'o',   'f'=>   'b',   'r'=>   'r',   'b'=>   'g', // 6个中心块， 下白上黄，左橙右红，前蓝后绿;名称及其对应颜色；
-    'dl'=>  'wo',  'df'=>  'wb',  'dr'=>  'wr',  'db'=>  'wg',  // 12个中间棱块之下层交界处，4个；名称及其对应颜色；
-    'ul'=>  'yo',  'uf'=>  'yb',  'ur'=>  'yr',  'ub'=>  'yg',  // 12个中间棱块之上层交界处，4个；
-    'lf'=>  'ob',  'fr'=>  'br',  'rb'=>  'rg',  'bl'=>  'go',  // 12个中间棱块之中层交界处，4个；
-    'dlf'=> 'wob', 'dfr'=> 'wbr', 'drb'=> 'wrg', 'dbl'=> 'wgo', // 8个角块之下层，4个；名称及其对应颜色；
-    'ulf'=> 'yob', 'ufr'=> 'ybr', 'urb'=> 'yrg', 'ubl'=> 'ygo'  // 8个角块之上层，4个；
-];
+The names of the facelet positions of the cube (letters stand for Up, Left, Front, Right, Back, and Down):
+             |************|
+             |*U1**U2**U3*|
+             |************|
+             |*U4**U5**U6*|
+             |************|
+             |*U7**U8**U9*|
+             |************|
+|************|************|************|************|
+|*L1**L2**L3*|*F1**F2**F3*|*R1**R2**R3*|*B1**B2**B3*|
+|************|************|************|************|
+|*L4**L5**L6*|*F4**F5**F6*|*R4**R5**R6*|*B4**B5**B6*|
+|************|************|************|************|
+|*L7**L8**L9*|*F7**F8**F9*|*R7**R8**R9*|*B7**B8**B9*|
+|************|************|************|************|
+             |************|
+             |*D1**D2**D3*|
+             |************|
+             |*D4**D5**D6*|
+             |************|
+             |*D7**D8**D9*|
+             |************|
 
-//魔方颜色
-$c = ['r'=>'red','g'=>'green','y'=>'yellow','o'=>'orange','b'=>'blue','w'=>'white'];
 
-$next = ['l'=>'f','f'=>'r','r'=>'b','b'=>'l'];      // left --> front --> right --> back --> left
-$prev = ['l'=>'b','b'=>'r','r'=>'f','f'=>'l'];      // 上面的反向链表
-$next_c = ['o'=>'b','b'=>'r','r'=>'g','g'=>'o'];    // next链表的颜色表示。如 left(orange) --> front(blue) --> right(red) --> back(green) --> left
+ */
 
-/**************************** 魔方基本动作 (小写字母是顺时针，大写字母是逆时针) ******************************/
-//back后面顺时针旋转90°
+// 定义6个面的结果字符串“面”顺序；默认完好的顺序是 上、右、前、下、左、后： UUUUUUUUU RRRRRRRRR FFFFFFFFF DDDDDDDDD LLLLLLLLL BBBBBBBBB
+$rlt_str_face_order = ['u','r','f','d','l','b'];  // 6个面字符串顺序, 就用小写
+$rlt_str_face_order = ['d','u','l','f','r','b'];
+
+
+// 定义6个面的颜色，通常是：上黄下白，前蓝后绿，左橙右红。用于拼装初始魔方状态
+//  上黄-yellow 下白-white 前蓝-blue 后绿-green 左橙-orange 右红-red
+$face_color = ['u' => 'y', 'd' => 'w', 'f' => 'b', 'b' => 'g', 'l' => 'o', 'r' => 'r'];
+
+// 魔方颜色，单字母同颜色映射表, TODO 这里暂时用不到，在html页面展示可能会用到
+//$colors = ['r'=>'red','g'=>'green','y'=>'yellow','o'=>'orange','b'=>'blue','w'=>'white'];
+
+
+// 定义6个面，顺序：下、上、左、前、右、后。通常人们都是先完成下层，再完成上层，所以下层放在前面 [就用上面即可，废弃下面的定义]
+//$face6_sort = ['d','u','l','f','r','b'];  // 6个面
+//$face6_flip = array_flip($face6_sort);    // 反转用于排序
+
+$play_action9 = array_merge($rlt_str_face_order, []);  // 'm','s','e' 除了6个面，还有前后转动的中层M、左右转动的中层S、水平转动的中层E
+
+// 魔方对象，共26块 = 6(个中心快) + 12(棱块) + 8(角块) ，存储魔方的状态。
+//$m = [
+//    'd'=>   'w',   'u'=>   'y',   'l'=>   'o',   'f'=>   'b',   'r'=>   'r',   'b'=>   'g', // 6个中心块， 下白上黄，前蓝后绿，左橙右红;名称及其对应颜色；
+//    'dl'=>  'wo',  'df'=>  'wb',  'dr'=>  'wr',  'db'=>  'wg',  // 12个中间棱块之下层交界处，4个；名称及其对应颜色；
+//    'ul'=>  'yo',  'uf'=>  'yb',  'ur'=>  'yr',  'ub'=>  'yg',  // 12个中间棱块之上层交界处，4个；
+//    'lf'=>  'ob',  'fr'=>  'br',  'rb'=>  'rg',  'lb'=>  'go',  // 12个中间棱块之中层交界处，4个；
+//    'dlf'=> 'wob', 'dfr'=> 'wbr', 'drb'=> 'wrg', 'dlb'=> 'wgo', // 8个角块之下层，4个；名称及其对应颜色；
+//    'ulf'=> 'yob', 'ufr'=> 'ybr', 'urb'=> 'yrg', 'ulb'=> 'ygo'  // 8个角块之上层，4个；
+//];
+$m = init_morefun($rlt_str_face_order, $face_color);    // 共26个元素，程序生成的跟上面一样，只是bl组合的顺序不一样。
+
+
+// 初始魔方状态
+function init_morefun($face6_arr, $face_color) {
+    $l_rlt = [];    // 参照$m
+    // 6个中心块
+    foreach ($face6_arr as $letter_1) {
+        $l_rlt[$letter_1] = $face_color[$letter_1]; // 6个中心块
+    }
+
+    // 12个棱块，按照上中下层; key为两个字母的组合，总共12种组合C6^2还要排除3对对面颜色不可能组合在一起：ud、lr、fb，
+    $num = count($face6_arr);
+    for ($i = 0; $i < $num; $i++) {
+        for ($j = $i+1; $j < $num; $j++) { // 排列组合，不分前后，所以可以$j = $i+1;
+            // 需要排除掉上下、前后、左右这三种情况ud、lr、fb，也就是说，有u就不能有d；同理有l就不能有r；有f就不能有b;
+            $key = $face6_arr[$i] . $face6_arr[$j]; // 棱块key名称需要固定一个顺序，例如ul和lu其实都是指左边和上面交界棱块。
+            if (false !== strpos($key, 'u') && false !== strpos($key, 'd')) continue;
+            if (false !== strpos($key, 'f') && false !== strpos($key, 'b')) continue;
+            if (false !== strpos($key, 'l') && false !== strpos($key, 'r')) continue;
+
+            $key = getSort($key);    // 返回稳定的排序，确保key唯一
+            if (isset($l_rlt[$key]))
+                continue;
+            $l_rlt[$key] = $face_color[$key[0]] . $face_color[$key[1]]; // 获取对应颜色字符串
+        }
+    }
+
+    // 8个角块，三个字母的组合，同时需要删除重复的
+    for ($i = 0; $i < $num; $i++) {
+        for ($j = $i+1; $j < $num; $j++) {
+            for ($m = $j+1; $m < $num; $m++) {  // 排列组合，不分前后，三个面需要不同所以可以$m = $j+1;
+                // 需要排除掉上下、前后、左右这三种ud、lr、fb字母同时出现的问题
+                $key = $face6_arr[$i] . $face6_arr[$j] . $face6_arr[$m]; // 棱块key名称需要固定一个顺序
+                if (false !== strpos($key, 'u') && false !== strpos($key, 'd')) continue;
+                if (false !== strpos($key, 'f') && false !== strpos($key, 'b')) continue;
+                if (false !== strpos($key, 'l') && false !== strpos($key, 'r')) continue;
+
+                $key = getSort($key);    // 返回稳定的排序，确保key唯一
+                if (isset($l_rlt[$key]))
+                    continue;
+                $l_rlt[$key] = $face_color[$key[0]] . $face_color[$key[1]] . $face_color[$key[2]]; // 获取对应颜色字符串
+            }
+        }
+    }
+
+    return $l_rlt;
+}
+
+
+// 初始状态, 6个面，颜色初始化, TODO 暂时用不上，页面展示可能会用上
+//$ob = [
+//    'u'=>[['y','y','y'],['y','y','y'],['y','y','y']],
+//    'l'=>[['o','o','o'],['o','o','o'],['o','o','o']],
+//    'f'=>[['b','b','b'],['b','b','b'],['b','b','b']],
+//    'r'=>[['r','r','r'],['r','r','r'],['r','r','r']],
+//    'b'=>[['g','g','g'],['g','g','g'],['g','g','g']],
+//    'd'=>[['w','w','w'],['w','w','w'],['w','w','w']]
+//];
+//$ob = init_ob($rlt_str_face_order, $face_color);
+
+// 6个面，颜色初始化
+function init_ob($face6_arr, $face_color) {
+    $l_arr = [];
+    // 6个面
+    foreach ($face6_arr as $letter_1) {
+        $l_color = [];
+        $one_c = $face_color[$letter_1];
+        $l_color[] = [$one_c, $one_c, $one_c];  // 第1行3个元素
+        $l_color[] = [$one_c, $one_c, $one_c];  // 第2行3个元素
+        $l_color[] = [$one_c, $one_c, $one_c];  // 第3行3个元素
+        $l_arr[$letter_1] = $l_color; // 每个面的3X3=9个元素的颜色数组
+    }
+    return $l_arr;
+}
+
+
+// TODO del
+function getResultStringFaceOrder() {
+    $l_order = $GLOBALS['rlt_str_face_order'];
+    return $GLOBALS['rlt_str_face_order']; // 可以做一些检查，避免
+}
+
+// 定义排序函数，用于字符串排序
+function compareStr($a, $b) {
+    $a = trim($a);
+    $b = trim($b);
+    $face_sort = $GLOBALS['rlt_str_face_order']; // 字母必须在这些指定的字母中
+    $face_flip = array_flip($face_sort);         // 键值互换
+    if (!in_array($a, $face_sort) || !in_array($b, $face_sort)) {
+        exit( $a . ' or '. $b . ' not in face_sort!');
+    }
+    // if ($face_flip[$a] == $face_flip[$b]) return 0; 此行可以去掉
+    return ($face_flip[$a] > $face_flip[$b]) ? 1 : -1;  // 升序:由小到大
+    //return ($face_flip[$a] > $face_flip[$b]) ? -1 : 1;  // 这是降序:由大到小
+}
+
+// 对字符串排序，例如角块Key的字母顺序。可应用于二维数组
+function getSort($str) {
+    $arr = str_split($str); // 字符串映射为一维数组
+    uasort($arr, 'compareStr'); // 排序后
+    $str = implode('', $arr);   // 转成字符串
+    return $str;
+}
+
+function filter_action($str) {
+    if (in_array(strtolower($str), $GLOBALS['play_action9']))
+        return true;
+    return false;
+}
+
+
+/**************************** 魔方基本动作 ******************************/
+//$m = [
+//    'd'=>   'w',   'u'=>   'y',   'l'=>   'o',   'f'=>   'b',   'r'=>   'r',   'b'=>   'g', // 6个中心块， 下白上黄，前蓝后绿，左橙右红;名称及其对应颜色；
+//    'dl'=>  'wo',  'df'=>  'wb',  'dr'=>  'wr',  'db'=>  'wg',  // 12个中间棱块之下层交界处，4个；名称及其对应颜色；
+//    'ul'=>  'yo',  'uf'=>  'yb',  'ur'=>  'yr',  'ub'=>  'yg',  // 12个中间棱块之上层交界处，4个；
+//    'lf'=>  'ob',  'fr'=>  'br',  'rb'=>  'rg',  'lb'=>  'go',  // 12个中间棱块之中层交界处，4个；
+//    'dlf'=> 'wob', 'dfr'=> 'wbr', 'drb'=> 'wrg', 'dlb'=> 'wgo', // 8个角块之下层，4个；名称及其对应颜色；
+//    'ulf'=> 'yob', 'ufr'=> 'ybr', 'urb'=> 'yrg', 'ulb'=> 'ygo'  // 8个角块之上层，4个；
+//];
+
+//背面顺时针旋转90°
 function mov_b() {
     global $m;
-    $tmp = $m['ub'];
-    $m['ub'] = $m['rb'];
+    // 背面中心块没有动；只有4个棱块和4个角块共8块位置发生变化。
+    $tmp = $m['ub'];        // 选择一个块临时存放，
+    $m['ub'] = $m['rb'];    // 上面背面被右背替换，
     $m['rb'] = $m['db'];
-    $m['db'] = $m['bl']; $m['db'] = $m['db'][1] + $m['db'][0];
-    $m['bl'] = $tmp;  $m['bl'] = $m['bl'][1] + $m['bl'][0];
+    $m['db'] = $m['bl'];
+    $m['db'] = $m['db'][1] + $m['db'][0];
+    $m['bl'] = $tmp;
+    $m['bl'] = $m['bl'][1] + $m['bl'][0];
     $tmp = $m['ubl'];
-    $m['ubl'] = $m['urb']; $m['ubl'] = $m['ubl'][1] + $m['ubl'][2] + $m['ubl'][0];
-    $m['urb'] = $m['drb']; $m['urb'] = $m['urb'][1] + $m['urb'][0] + $m['urb'][2];
-    $m['drb'] = $m['dbl']; $m['drb'] = $m['drb'][2] + $m['drb'][0] + $m['drb'][1];
-    $m['dbl'] = $tmp;   $m['dbl'] = $m['dbl'][2] + $m['dbl'][1] + $m['dbl'][0];
+    $m['ubl'] = $m['urb'];
+    $m['ubl'] = $m['ubl'][1] + $m['ubl'][2] + $m['ubl'][0];
+    $m['urb'] = $m['drb'];
+    $m['urb'] = $m['urb'][1] + $m['urb'][0] + $m['urb'][2];
+    $m['drb'] = $m['dbl'];
+    $m['drb'] = $m['drb'][2] + $m['drb'][0] + $m['drb'][1];
+    $m['dbl'] = $tmp;
+    $m['dbl'] = $m['dbl'][2] + $m['dbl'][1] + $m['dbl'][0];
 }
 
 
@@ -40,13 +208,19 @@ function mov_r() {
     $tmp = $m['ur'];
     $m['ur'] = $m['fr'];
     $m['fr'] = $m['dr'];
-    $m['dr'] = $m['rb']; $m['dr'] = $m['dr'][1] + $m['dr'][0];
-    $m['rb'] = $tmp;  $m['rb'] = $m['rb'][1] + $m['rb'][0];
+    $m['dr'] = $m['rb'];
+    $m['dr'] = $m['dr'][1] + $m['dr'][0];
+    $m['rb'] = $tmp;
+    $m['rb'] = $m['rb'][1] + $m['rb'][0];
     $tmp = $m['urb'];
-    $m['urb'] = $m['ufr']; $m['urb'] = $m['urb'][1] + $m['urb'][2] + $m['urb'][0];
-    $m['ufr'] = $m['dfr']; $m['ufr'] = $m['ufr'][1] + $m['ufr'][0] + $m['ufr'][2];
-    $m['dfr'] = $m['drb']; $m['dfr'] = $m['dfr'][2] + $m['dfr'][0] + $m['dfr'][1];
-    $m['drb'] = $tmp;   $m['drb'] = $m['drb'][2] + $m['drb'][1] + $m['drb'][0];
+    $m['urb'] = $m['ufr'];
+    $m['urb'] = $m['urb'][1] + $m['urb'][2] + $m['urb'][0];
+    $m['ufr'] = $m['dfr'];
+    $m['ufr'] = $m['ufr'][1] + $m['ufr'][0] + $m['ufr'][2];
+    $m['dfr'] = $m['drb'];
+    $m['dfr'] = $m['dfr'][2] + $m['dfr'][0] + $m['dfr'][1];
+    $m['drb'] = $tmp;
+    $m['drb'] = $m['drb'][2] + $m['drb'][1] + $m['drb'][0];
 }
 
 //前面顺时针旋转90°
@@ -55,13 +229,19 @@ function mov_f() {
     $tmp = $m['uf'];
     $m['uf'] = $m['lf'];
     $m['lf'] = $m['df'];
-    $m['df'] = $m['fr']; $m['df'] = $m['df'][1] + $m['df'][0];
-    $m['fr'] = $tmp;  $m['fr'] = $m['fr'][1] + $m['fr'][0];
+    $m['df'] = $m['fr'];
+    $m['df'] = $m['df'][1] + $m['df'][0];
+    $m['fr'] = $tmp;
+    $m['fr'] = $m['fr'][1] + $m['fr'][0];
     $tmp = $m['ufr'];
-    $m['ufr'] = $m['ulf']; $m['ufr'] = $m['ufr'][1] + $m['ufr'][2] + $m['ufr'][0];
-    $m['ulf'] = $m['dlf']; $m['ulf'] = $m['ulf'][1] + $m['ulf'][0] + $m['ulf'][2];
-    $m['dlf'] = $m['dfr']; $m['dlf'] = $m['dlf'][2] + $m['dlf'][0] + $m['dlf'][1];
-    $m['dfr'] = $tmp;   $m['dfr'] = $m['dfr'][2] + $m['dfr'][1] + $m['dfr'][0];
+    $m['ufr'] = $m['ulf'];
+    $m['ufr'] = $m['ufr'][1] + $m['ufr'][2] + $m['ufr'][0];
+    $m['ulf'] = $m['dlf'];
+    $m['ulf'] = $m['ulf'][1] + $m['ulf'][0] + $m['ulf'][2];
+    $m['dlf'] = $m['dfr'];
+    $m['dlf'] = $m['dlf'][2] + $m['dlf'][0] + $m['dlf'][1];
+    $m['dfr'] = $tmp;
+    $m['dfr'] = $m['dfr'][2] + $m['dfr'][1] + $m['dfr'][0];
 }
 
 //左面顺时针旋转90°
@@ -70,13 +250,19 @@ function mov_l() {
     $tmp = $m['ul'];
     $m['ul'] = $m['bl'];
     $m['bl'] = $m['dl'];
-    $m['dl'] = $m['lf']; $m['dl'] = $m['dl'][1] + $m['dl'][0];
-    $m['lf'] = $tmp;  $m['lf'] = $m['lf'][1] + $m['lf'][0];
+    $m['dl'] = $m['lf'];
+    $m['dl'] = $m['dl'][1] + $m['dl'][0];
+    $m['lf'] = $tmp;
+    $m['lf'] = $m['lf'][1] + $m['lf'][0];
     $tmp = $m['ulf'];
-    $m['ulf'] = $m['ubl']; $m['ulf'] = $m['ulf'][1] + $m['ulf'][2] + $m['ulf'][0];
-    $m['ubl'] = $m['dbl']; $m['ubl'] = $m['ubl'][1] + $m['ubl'][0] + $m['ubl'][2];
-    $m['dbl'] = $m['dlf']; $m['dbl'] = $m['dbl'][2] + $m['dbl'][0] + $m['dbl'][1];
-    $m['dlf'] = $tmp;   $m['dlf'] = $m['dlf'][2] + $m['dlf'][1] + $m['dlf'][0];
+    $m['ulf'] = $m['ubl'];
+    $m['ulf'] = $m['ulf'][1] + $m['ulf'][2] + $m['ulf'][0];
+    $m['ubl'] = $m['dbl'];
+    $m['ubl'] = $m['ubl'][1] + $m['ubl'][0] + $m['ubl'][2];
+    $m['dbl'] = $m['dlf'];
+    $m['dbl'] = $m['dbl'][2] + $m['dbl'][0] + $m['dbl'][1];
+    $m['dlf'] = $tmp;
+    $m['dlf'] = $m['dlf'][2] + $m['dlf'][1] + $m['dlf'][0];
 }
 
 //顶面顺时针旋转90°
@@ -161,77 +347,6 @@ function mov_d() {
             mov($com[$i]);
         }
     }
-
-    /*************************** 层先法复原魔方 **************************/
-    //查找块所在的位置及状态, TODO 暂不需要复原整个魔方
-//    function pos($block) {
-//        global $m;
-//        $reg = '/[' . $block . ']{' . count($block) . '}/i';
-//        foreach ($m as $k => $l_val) {
-//            if (preg_match($reg, $m[$k])) {
-//                return ['k' => $k, 'v' => $m[$k]];
-//            }
-//        }
-//        echo 'something error! in pos';
-//        return ['k' => '', 'v' =>''];
-//    }
-
-    //调整单个底棱块
-//    function _step_1($position, $block) {
-//        global $m,$next,$next_c;
-//        $exp = '';
-//        $exp_log = '';
-//        $s = '';
-//        for($i = 0; $i < 7; $i++){
-//            $s = pos($block);
-//            if (false !== strpos($s['k'], 'd')) {
-//                if($s['v'][0] == $block[0]){
-//                    if($s['k'] == $position){
-//                        //console.log(exp_log);
-//                        return $exp_log;		//最终返回指令
-//                    }else{
-//                        $exp = $s['k'][1] + $s['k'][1];
-//                    }
-//                }else{
-//                    $exp = $s['k'][1];
-//                }
-//            }else if(false !== strpos($s['k'], 'u')){
-//                if($s['k'][1] == $position[1]){
-//                    if($s['v'][0] == $block[0]){
-//                        $exp = $s['k'][1] + $s['k'][1];
-//                    }else if($m[$position[0] . $next[$s['k'][1]]] != $m[$position[0]] . $m[$next[$s['k'][1]]]){
-//                        $exp = 'U' . $next[$s['k'][1]].toUpperCase() . $s['k'][1];
-//                    }else{
-//                        $exp = 'U' . $next[$s['k'][1]].toUpperCase() . $s['k'][1] . $next[$s['k'][1]];
-//                    }
-//                }else{
-//                    $exp = 'u';
-//                }
-//            }else{
-//                if($s['v'][0] == $block[0]){
-//                    if($s['k'][1] == $position[1]){
-//                        $exp = strtoupper($s['k'][1]);
-//                    }else if($m[$position[0] . $s['k'][1]] != $m[$position[0]] . $m[$s['k'][1]]){
-//                        $exp = $s['k'][1];
-//                    }else{
-//                        $exp = $s['k'][1] . 'u' . strtoupper($s['k'][1]);
-//                    }
-//                }else{
-//                    if($s['k'][0] == $position[1]){
-//                        $exp = $s['k'][0];
-//                    }else if($m[$position[0]+$s['k'][0]] != $m[$position[0]]+$m[$s['k'][0]]){
-//                        $exp = strtoupper($s['k'][0]);
-//                    }else{
-//                        $exp = strtoupper($s['k'][0]) . 'u' . $s['k'][0];
-//                    }
-//                }
-//            }
-//            $exp_log .= $exp;
-//            exe($exp);
-//        }
-//        //console.log('[1'+$exp_log+'1]');
-//        return '[1' . $exp_log . '1]';
-//    }
 
     //压缩指令数,一正一反、旋转4圈等都相当于没有旋转；顺时针旋转3圈相当于逆时针旋转一圈
     function reduce($str) {
--- rubikcube.php
+++ rubikcube.php
@@ -3,14 +3,65 @@
  输入转动步骤，得到字符串结果，(小写字母是顺时针，大写字母是逆时针)
  php rubikcube.php -a "f r u R U F"
 
+ php rubikcube.php -o "urfdlb" -a "f r u R U F"  -- 指定排序，动作组合
+
  */
 include_once 'morenfun.cls.php';
 
-$o = getopt('a:');
-main($o);
+$option = getopt('a:b:o:');
 
-function main($o) {
-    return 1;
+
+
+
+main($option);
+
+function main($o)
+{
+    // 1. 参数检查，
+    //   1) action参数，转动动作字母是否在指定动作限定内，不认识的动作，直接删除掉
+    $str = (isset($o['a']) && $o['a']) ? $o['a'] : '';
+    if (!$str) {
+        echo ' -a 必须提供操作动作，如：php rubikcube.php -a "f r u R U F"' . "\r\n";
+        return '';
+    }
+    //   2) 结果顺序order参数, 主要是结果顺序
+    $order_str = (isset($o['o']) && $o['o']) ? $o['o'] : 'urfdlb';
+    //   3) 初始状态
+    $begin_ob = (isset($o['b']) && $o['b']) ? $o['b'] : $GLOBALS['ob'];
+
+    // 2. 参数过滤，如果出现了不被识别的动作，过滤掉，并不给出提示。TODO 第一期只允许单字母动作,U2,U3-这种旋转180°，270°(逆时针)放到下期
+    $str = str_replace(' ', '', $str);  // 去掉空白字符
+    $str = strtolower($str);            // 全部转成小写字母，大小写暂不区分
+    $action_arr = str_split($str);
+    $action_arr = array_filter($action_arr, 'filter_action');   // 限定动作
+
+    // 3. 初始状态：通常默认是完好的 $begin_ob，也可以参数指定。
+
+    print_r($action_arr);
+    // 4. 逐个动作执行
+    foreach ($action_arr as $l_act_val) {
+        //
+    }
+
+    print_r($GLOBALS['m']);
+
+
+    // 字符串排序
+    // $str2 = getSort($str2);
+
+
+    return '';
 }
 
-print_r($m);
+//print_r($m);
+// 按照 $face_sort 中的顺序，并且字母只能在这6个字母
+//echo 'before: '. $str . "\r\n";
+//$str = str_replace(' ', '', $str);
+//$str = strtolower($str);
+//echo 'strtolower: '. $str . "\r\n";
+//$str = getSort($str);
+//echo 'after: '. $str . "\r\n";
+
+// 结果字符串，默认完好的顺序是 上、右、前、下、左、后： UUUUUUUUU RRRRRRRRR FFFFFFFFF DDDDDDDDD LLLLLLLLL BBBBBBBBB
+$rlt_str_order_arr = getResultStringFaceOrder();
+//$rlt_str_face_order = ['u','r','f','d','l','b'];  // 6个面字符串顺序，也可以改顺序
